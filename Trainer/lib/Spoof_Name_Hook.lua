d535ffa93612378dce6050887a00dacf44084ae0
s10715 <91897764+s10715@users.noreply.github.com>
2026-09-08 17:09:56 +0800

    update

--- Trainer/lib/Spoof_Name_Hook.lua
+++ Trainer/lib/Spoof_Name_Hook.lua
@@ -1,7 +1,4 @@
--- deprecated, now only change your name locally, the other still see your real name, need to update
-
-
-
+-- only work as client. (if you're host, then only change your name locally, so make it invalid as host.)
 
 -- doesn't change your name locally in the end-game menu (where you pick cards), though others still see the spoofed name.
 local spoof_name = "Unknown"
@@ -13,12 +10,31 @@ local function get_real_name()
 	end
 end
 
-local function set_spoof_name()
-	if managers.network and managers.network:session() and managers.network:session():local_peer() then
-		local my_peer = managers.network:session():local_peer()
-		if my_peer then
-			my_peer:set_name(spoof_name)
+
+if NetworkPeer then
+	orig_NetworkPeer_name = orig_NetworkPeer_name or NetworkPeer.name
+	function NetworkPeer:name()
+		local session = managers.network and managers.network:session()
+		if spoof_name and Network:is_client() and session and self == session:local_peer() then
+			return spoof_name
 		end
+		return orig_NetworkPeer_name(self)
+	end
+
+	orig_NetworkPeer_set_name = orig_NetworkPeer_set_name or NetworkPeer.set_name
+	function NetworkPeer:set_name(name)
+		local session = managers.network and managers.network:session()
+		if spoof_name and Network:is_client() and session and self == session:local_peer() then
+			name = spoof_name
+		end
+		return orig_NetworkPeer_set_name(self, name)
+	end
+end
+
+local function set_spoof_name()
+	local session = managers.network and managers.network:session()
+	if spoof_name and Network:is_client() and session and session:local_peer() then
+		session:local_peer():set_name(spoof_name)
 		--[[
 		-- deprecated
 		for _, peer in pairs(managers.network:session()._peers) do
